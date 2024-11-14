3a17e8aa86d3b729fc998d29015da2ca25bf89f0
BAHATTIN KOC <bahattin.koc@atmosware.turkcell.com.tr>
2024-11-14 16:01:57 +0300

    First commit Project created

---
+++ globuddy/globuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  globuddyApp.swift
+//  globuddy
+//
+//  Created by BAHATTIN KOC on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct globuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
